89831a7567ff094b6187fddd96172ab3dd6cb835
Owen Wang <owenwang0099@gmail.com>
2024-06-04 20:28:21 -0400

    minor changes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -12,8 +12,7 @@ const Login = () => {
                     <div className="text-center lg:text-left lg:ml-8 w-min">
                         <h1 className="text-5xl font-bold">Internsheet</h1>
                         <p className="py-6">
-                            Track your job applications and race your friends to
-                            see who can send the most!
+                            Track your internship applications
                         </p>
                     </div>
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-8">
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,9 @@ const Sidebar = () => {
     const [season, setSeason] = useState("");
     const [location, setLocation] = useState("");
 
-    const addJob = async () => {
+    const jobSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
         const user = await supabase.auth.getUser();
 
         const { error } = await supabase.from("jobs").insert({
@@ -22,88 +24,100 @@ const Sidebar = () => {
             location: location,
         });
         if (error) console.log(error);
+
+        // Clear input fields
+        [...document.getElementsByClassName("jobInput")].forEach(
+            (input: Element) => ((input as HTMLInputElement).value = "")
+        );
     };
 
     return (
         <div>
-            <div className="flex flex-col items-center p-2 border rounded-2xl w-min">
+            <div
+                id="job-form"
+                className="flex flex-col items-center p-2 border rounded-2xl w-min"
+            >
                 <label>Add Job</label>
-                <input
-                    type="text"
-                    placeholder="Link to job posting"
-                    className="input input-bordered w-full max-w-xs my-2"
-                    onChange={(event) => setUrl(event.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Job Title"
-                    className="input input-bordered w-full max-w-xs my-2"
-                    onChange={(event) => setTitle(event.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Company"
-                    className="input input-bordered w-full max-w-xs my-2"
-                    onChange={(event) => setCompany(event.target.value)}
-                />
-                <div className="join my-2">
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="remote"
-                        aria-label="Office"
-                        onClick={() => setRemote("Office")}
-                    />
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="remote"
-                        aria-label="Hybrid"
-                        onClick={() => setRemote("Hybrid")}
-                    />
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="remote"
-                        aria-label="Remote"
-                        onClick={() => setRemote("Remote")}
-                    />
-                </div>
-                <div className="join my-2">
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="season"
-                        aria-label="Summer"
-                        onClick={() => setSeason("Summer")}
-                    />
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="season"
-                        aria-label="Fall"
-                        onClick={() => setSeason("Fall")}
-                    />
-                    <input
-                        className="join-item btn"
-                        type="radio"
-                        name="season"
-                        aria-label="Winter"
-                        onClick={() => setSeason("Winter")}
-                    />
-                </div>
-                <input
-                    type="text"
-                    placeholder="Location"
-                    className="input input-bordered w-full max-w-xs my-2"
-                    onChange={(event) => setLocation(event.target.value)}
-                />
-                <button
-                    onClick={addJob}
-                    className="bg-white text-black px-4 py-1 border rounded-lg m-4"
+                <form
+                    onSubmit={jobSubmit}
+                    className="flex flex-col items-center"
                 >
-                    Add
-                </button>
+                    <input
+                        type="text"
+                        placeholder="Link to job posting"
+                        className="jobInput input input-bordered w-full max-w-xs my-2"
+                        onChange={(event) => setUrl(event.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Job Title"
+                        className="jobInput input input-bordered w-full max-w-xs my-2"
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Company"
+                        className="jobInput input input-bordered w-full max-w-xs my-2"
+                        onChange={(event) => setCompany(event.target.value)}
+                    />
+                    <div className="join my-2">
+                        <input
+                            className="join-item btn"
+                            type="radio"
+                            name="remote"
+                            aria-label="Office"
+                            onClick={() => setRemote("Office")}
+                        />
+                        <input
+                            className="join-item btn"
+                            type="radio"
+                            name="remote"
+                            aria-label="Hybrid"
+                            onClick={() => setRemote("Hybrid")}
+                        />
+                        <input
+                            className="join-item btn"
+                            type="radio"
+                            name="remote"
+                            aria-label="Remote"
+                            onClick={() => setRemote("Remote")}
+                        />
+                    </div>
+                    <div className="join my-2">
+                        <input
+                            className="join-item btn"
+                            type="radio"
+                            name="season"
+                            aria-label="Summer"
+                            onClick={() => setSeason("Summer")}
+                        />
+                        <input
+                            className="join-item btn"
+                            type="radio"
+                            name="season"
+                            aria-label="Fall"
+                            onClick={() => setSeason("Fall")}
+                        />
+                        <input
+                            className="join-item btn"
+                            type="radio"
+                            name="season"
+                            aria-label="Winter"
+                            onClick={() => setSeason("Winter")}
+                        />
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Location"
+                        className="jobInput input input-bordered w-full max-w-xs my-2"
+                        onChange={(event) => setLocation(event.target.value)}
+                    />
+                    <input
+                        type="submit"
+                        value="Add"
+                        className="btn btn-outline self-end"
+                    />
+                </form>
             </div>
         </div>
     );
